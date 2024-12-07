83f98db755ec81cbd695560a960229602553ad1d
phamtoan <phamxuantoandc@gmail.com>
2024-12-07 09:31:39 +0700

    modify modbus_client.py

--- modbus_client.py
+++ modbus_client.py
@@ -1,8 +1,7 @@
-import serial
+from abc import ABC,abstractmethod
 import minimalmodbus
-import os
-import stat
-from logfile import LogFile
+import serial
+import time
 
 class Modbus_Function_Code:
     #input
@@ -13,21 +12,55 @@ class Modbus_Function_Code:
     WriteHoldRegs = 16
     WriteHoldReg = 6
 
-import time
-class Modbus_Serial_Client:
-    def __init__(self,port_mb:str,baudrate:int,slave_id:int,timeout:int,log:LogFile) -> None:
-        self.port = port_mb
-        self.slave_id = slave_id
-        self.baudrate = baudrate
-        self.timeout_modbus = timeout
+class Modbus_Client(ABC):
+    def __init__(self,slave_id:int,number_of_input:int,number_of_hold:int,timeout_modbus:int) -> None:
+        super().__init__()
+        self.timeout_modbus = timeout_modbus
         self.timeout_response = 10
-        self.log = log
+        self._number_of_input = number_of_input
+        self._number_of_hold = number_of_hold
+        self._modbus_error = True
+        self._input_regs = []
+        self._hold_regs = [0] * self._number_of_hold
+        self._slave_id = slave_id
+
+    @abstractmethod
+    def connect_to_server(self) -> bool:
+        pass
+
+    @abstractmethod
+    def read_input_regs(self):
+        pass
+
+    @abstractmethod
+    def write_hold_regs(self):
+        pass
+
+    @abstractmethod
+    def poll_server(self):
+        pass
+    @property
+    def number_of_input(self):
+        return self._number_of_input
+    @property
+    def number_of_hold(self):
+        return self._number_of_hold
+    @property 
+    def slave_id(self):
+        return self._slave_id
+    
+class Modbus_Serial_Client(Modbus_Client):
+    def __init__(self, slave_id:int, number_of_input: int, number_of_hold: int,port:str,baudrate:int,timeout_modbus:int) -> None:
+        super().__init__(slave_id,number_of_input, number_of_hold)
+        self.port = port
+        self.baudrate = baudrate
         
         
-    def connnect_modbus_server(self) -> bool:
+    def connect_to_server(self) -> bool:
+        super().connect_to_server()
         try:
-            os.chmod(self.port,stat.S_IRWXO)
-            self.client = minimalmodbus.Instrument(port=self.port,slaveaddress=self.slave_id,mode=minimalmodbus.MODE_RTU)
+            # os.chmod(self.port,stat.S_IRWXO)
+            self.client = minimalmodbus.Instrument(port=self.port,slaveaddress=self._slave_id,mode=minimalmodbus.MODE_RTU)
             self.client.serial.baudrate = self.baudrate
             self.client.serial.bytesize = 8
             self.client.serial.parity = serial.PARITY_NONE
@@ -35,9 +68,41 @@ class Modbus_Serial_Client:
             self.client.serial.stopbits = 1
             return True
         except Exception as e:
-            self.log.writeLog(type_log='error',msg = "Error : " + str(e))
+            # self.logfile.writeLog(type_log='error',msg=str(e))
+            print('error : ' + str(e))
+            return False
+        
+        return True
+    
+    def read_input_regs(self,address:int,count:int) -> list | None:
+        super().read_input_regs()
+        try:
+            value = self.client.read_registers(registeraddress=address,number_of_registers=count,functioncode=Modbus_Function_Code.ReadInputRegs)
+            return value
+        except Exception as e:
+            print(str(e))
+            return None
+    
+    def write_hold_regs(self,address:int,value:list) -> None:
+        super().write_hold_regs()
+        try:
+            self.client.write_registers(registeraddress=address,values=value)
+            return True
+        except Exception as e:
+            print(str(e))
             return False
     
+    def poll_server(self):
+        super().poll_server()
+        while True:
+            time.sleep(1)
 
-    #custom
-    #set rgb
+            
+        
+
+mb_client = Modbus_Serial_Client(number_of_input=10,number_of_hold=10,port='COM1',baudrate=115200)
+mb_client.connect_to_server()
+
+
+
+
